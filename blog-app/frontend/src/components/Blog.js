70ae537a5c284527883e3c9222410da7441cc6c9
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-10-22 00:58:56 +0300

    WIP: 5.7

--- blog-app/frontend/src/components/Blog.js
+++ blog-app/frontend/src/components/Blog.js
@@ -1,7 +1,51 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const BlogSummary = (props) => (
+    <div>{blog.title} {blog.author} {props.children}</div>
+  )
+
+  // TODO: add "like" button
+  const BlogExpanded = (props) => (
+    <div>{blog.title} {blog.author} {props.children}<br />
+      {blog.url}<br />
+      likes {blog.likes} ADD LIKE BUTTON HERE!!<br />
+      {blog.user.name}
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <BlogSummary>
+          <button onClick={toggleVisibility}>View</button>
+        </BlogSummary>
+      </div>
+      <div style={showWhenVisible}>
+        <BlogExpanded>
+          <button onClick={toggleVisibility}>Hide</button>
+        </BlogExpanded>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
