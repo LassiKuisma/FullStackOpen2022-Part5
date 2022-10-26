8b1e5f7d34550df475e245ec1e3966ad980ef324
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-10-26 21:22:51 +0300

    exercise 5.14

--- blog-app/frontend/src/components/Blog.test.js
+++ blog-app/frontend/src/components/Blog.test.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-    test('by default displays title and author but not url or likes', () => {
+    let container
+
+    beforeEach(() => {
         const blog = {
             title: 'blog title',
             author: 'blog author',
             url: 'blog url',
             likes: 'blog likes',
         }
+        container = render(
+            <Blog blog={blog} />
+        ).container
+    })
 
-        const { container } = render(<Blog blog={blog} />)
+    test('by default displays title and author but not url or likes', () => {
         const summary = container.querySelector('.blogSummary')
         const expanded = container.querySelector('.blogExpanded')
 
         expect(summary).not.toHaveStyle('display: none')
         expect(expanded).toHaveStyle('display: none')
+    })
+
+    test('after expanding blog view, url and likes are shown', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('View')
+        await user.click(button)
+
+
+        const summary = container.querySelector('.blogSummary')
+        expect(summary).toHaveStyle('display: none')
+
+        const expanded = container.querySelector('.blogExpanded')
+        expect(expanded).not.toHaveStyle('display: none')
     })
 })
